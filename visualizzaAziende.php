bca8a55152b6879c0b16e38aec0121ab879b2e08
Giacomo Minello <minellogiacomo@gmail.com>
2019-04-10 17:05:23 +0200

    Signed-off-by: Giacomo Minello <minellogiacomo@gmail.com>

--- visualizzaAziende.php
+++ visualizzaAziende.php
@@ -16,11 +16,28 @@ if (!isset($_SESSION["email"]) or !isset($_SESSION["password"])){
 <?php
 $object = new Business();
 $res = $object -> getInfoAziende();
+echo'<div class="book-taxi-section">  <div class="container"> 	<div class="section-header section-header-white">
+			<h6>Informazioni Aziende </h6></div> <form class="row" >';
 while ($row=$res->fetch(PDO::FETCH_ASSOC)) {
-    echo '<div>';
-    echo $row['NOME'];
-    echo '</div>';
+    echo ' <div class="form-group col-lg-4 col-md-6">
+                <label>Nome Azienda</label>
+                <input type="text"  Value="'.$row['NOME'].'" readonly class="form-control"/>
+     </div>';
+    echo ' <div class="form-group col-lg-4 col-md-6">
+                <label>Indirizzo</label>
+                <input type="text"  Value="'.$row['INDIRIZZO'].'" readonly class="form-control"/>
+     </div>';
+    echo ' <div class="form-group col-lg-4 col-md-6">
+                <label>Telefono</label>
+                <input type="text"  Value="'.$row['TELEFONO'].'" readonly class="form-control"/>
+     </div>';
+    echo ' <div class="form-group col-lg-4 col-md-6">
+                <label>Recapito</label>
+                <input type="text"  Value="'.$row['RECAPITO'].'" readonly class="form-control"/>
+     </div>';
+    echo'<hr>...</hr>';
 }
+echo'</form></div></div>';
 
 ?>
 <?php include('visualizzaAziende.html');?>
