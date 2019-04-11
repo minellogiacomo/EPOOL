c76653b0f7aa97ff6040cfd9cdcdf9fd1d85c3a2
Giacomo Minello <minellogiacomo@gmail.com>
2019-04-11 19:15:04 +0200

    Signed-off-by: Giacomo Minello <minellogiacomo@gmail.com>

--- connectionMongo.php
+++ connectionMongo.php
@@ -1,6 +1,8 @@
 <?php
+echo extension_loaded("mongodb") ? "loaded\n" : "not loaded\n";
 try {
     $mng = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+    print_r($mng);
     $bulk = new MongoDB\Driver\BulkWrite();
     $doc = ['_id' => new MongoDB\BSON\ObjectID(), 'name' => 'Toyota'];
     $bulk->insert($doc);
